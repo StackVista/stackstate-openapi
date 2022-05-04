778b774871426b84c39b81b44febaae80139c9d8
Bram Schuur <bschuur@stackstate.com>
2022-05-04 11:40:07 +0200

    STAC-16415: Add linting and validating

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "StackState OpenApi";
+
+  nixConfig.bash-prompt = "STS OpenApi 2 $ ";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+
+        devShell = pkgs.mkShell {
+          buildInputs = (with pkgs; [
+            openapi-generator-cli
+            nodejs-14_x
+            bash
+          ]);
+        };
+      });
+}
