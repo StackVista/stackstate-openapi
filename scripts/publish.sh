09ee77ef5f07b27902997dea74ea6b76b39d8194
Jeroen van Erp <jeroen@hierynomus.com>
2022-06-23 17:02:59 +0200

    STAC-0: Do not publish for RCs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,13 +2,15 @@
 set -e && cd "$(dirname "$0")" && cd ../
 
 if [ -z "$1" ]
-then 
+then
   echo "Please enter a version number as a first argument to this script, e.g. 'publish.sh v1.0.3'"
   exit 1
 fi
 
 VERSION=$1
 
+MAJOR_MINOR=$(echo "$VERSION" | cut -d '.' -f 1-2)
+OUTPUT_FILE="openapi-$VERSION.html"
 
 checkForVariable() {
   if [[ -z ${!1+set} ]]; then
@@ -17,13 +19,13 @@ checkForVariable() {
   fi
 }
 
-checkForVariable AWS_DEFAULT_REGION
-checkForVariable AWS_ACCESS_KEY_ID
-checkForVariable AWS_SECRET_ACCESS_KEY
+# checkForVariable AWS_DEFAULT_REGION
+# checkForVariable AWS_ACCESS_KEY_ID
+# checkForVariable AWS_SECRET_ACCESS_KEY
 
-redoc-cli bundle spec/openapi.yaml -o dist/index.html
+redoc-cli bundle spec/openapi.yaml -o "dist/$OUTPUT_FILE"
 
-DEST="s3://cli-dl.stackstate.com/stackstate-openapi/$VERSION/index.html"
-echo "Uploading docs version $VERSION to $DEST" 
-aws s3 cp dist/index.html $DEST 
-echo "API docs now available on: https://dl.stackstate.com/stackstate-openapi/$VERSION/index.html"
+DEST="s3://cli-dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$OUTPUT_FILE"
+echo "Uploading docs version $VERSION to $DEST"
+aws s3 cp "dist/$OUTPUT_FILE" "$DEST"
+echo "API docs now available on: https://dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$OUTPUT_FILE"
