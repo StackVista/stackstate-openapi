4ba3e22234394c226470bd7f9dc7c177b74523db
Kajetan Rzepecki <k@spartan.works>
2022-09-27 12:50:12 +0200

    Extracted the Admin API into a separate OpenAPI root spec.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,7 +10,8 @@ fi
 VERSION=$1
 
 MAJOR_MINOR=$(echo "$VERSION" | cut -d '.' -f 1-2)
-OUTPUT_FILE="openapi-$VERSION.html"
+MAIN_OUTPUT_FILE="openapi-$VERSION.html"
+ADMIN_OUTPUT_FILE="openapi-admin-$VERSION.html"
 
 checkForVariable() {
   if [[ -z ${!1+set} ]]; then
@@ -23,9 +24,15 @@ checkForVariable() {
 # checkForVariable AWS_ACCESS_KEY_ID
 # checkForVariable AWS_SECRET_ACCESS_KEY
 
-redoc-cli bundle spec/openapi.yaml -o "dist/$OUTPUT_FILE"
+redoc-cli bundle spec/openapi.yaml -o "dist/$MAIN_OUTPUT_FILE"
+redoc-cli bundle spec/openapi_admin.yaml -o "dist/$ADMIN_OUTPUT_FILE"
 
-DEST="s3://cli-dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$OUTPUT_FILE"
-echo "Uploading docs version $VERSION to $DEST"
-aws s3 cp "dist/$OUTPUT_FILE" "$DEST"
-echo "API docs now available on: https://dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$OUTPUT_FILE"
+MAIN_DEST="s3://cli-dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$MAIN_OUTPUT_FILE"
+echo "Uploading docs version $VERSION to $MAIN_DEST"
+aws s3 cp "dist/$MAIN_OUTPUT_FILE" "$MAIN_DEST"
+echo "API docs now available on: https://dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$MAIN_OUTPUT_FILE"
+
+ADMIN_DEST="s3://cli-dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$ADMIN_OUTPUT_FILE"
+echo "Uploading docs version $VERSION to $ADMIN_DEST"
+aws s3 cp "dist/$ADMIN_OUTPUT_FILE" "$ADMIN_DEST"
+echo "Admin API docs now available on: https://dl.stackstate.com/stackstate-openapi/$MAJOR_MINOR/$ADMIN_OUTPUT_FILE"
